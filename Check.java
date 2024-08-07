a68640157f4171d1dfda4ecd0a0eb619ebdd3522
annawarpe <156175611+annawarpe@users.noreply.github.com>
2024-08-07 06:02:01 -0700

    Check.java

---
+++ Check.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+class Check
+{
+public static void main(String args[])
+{
+int i=5;
+if(i==0)
+{
+System.out.println("num is Zero");
+}
+else if(i<0)
+{
+System.out.println("num is negative");
+}
+else
+{
+System.out.println("num is positive");
+}
+}
+}
