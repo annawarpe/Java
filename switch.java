12a94fb7130e8fd43b72ed53aca42de27194e67d
annawarpe <156175611+annawarpe@users.noreply.github.com>
2024-08-08 02:56:23 -0700

    switch.java

---
+++ switch.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+class switsh
+{
+public static void main(String args[])
+{
+int num='x';
+switch(num)
+{
+case'a':System.out.println("hi");
+break;
+case'b':System.out.println("Hello");
+break;
+case'c':System.out.println("i am Annasaheb warpe");
+break;
+default:System.out.println("not matched any case");
+}
+}
+}
