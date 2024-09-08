6e07f7e322a1af45a6963efe8a78919b8e98b30d
annawarpe <156175611+annawarpe@users.noreply.github.com>
2024-09-08 05:54:57 -0700

    Add files via upload

---
+++ exception.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+class exception
+{
+public static void main(String str[])
+{
+System.out.println("hello welcome to my program");
+int a=10,b=0,c;
+try
+{
+c=a/b;
+System.out.print(c);
+}
+catch(Exception e)
+{
+System.out.println(" ");
+}
+
+System.out.println("end of the program");
+}
+}
